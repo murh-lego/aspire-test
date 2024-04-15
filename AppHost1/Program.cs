fab7af8cdeb6681f72301b8d674dbe7c1a85f255
Marko Urh (ext) <marko.urh@consultant.lego.com>
2024-04-15 12:05:04 +0200

    no message

--- AppHost1/Program.cs
+++ AppHost1/Program.cs
@@ -1,5 +1,7 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<Projects.WebApplication1>("webapplication1");
+builder
+    .AddProject<Projects.WebApplication1>("webapplication1")
+    .WithExternalHttpEndpoints();
 
 builder.Build().Run();
